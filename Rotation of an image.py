0976d198d793da2b1b84ba780baa882d3b0108e3
s44nidhya <140419811+s44nidhya@users.noreply.github.com>
2024-11-12 18:01:58 +0530

    Update Rotation of an image.py

--- Rotation of an image.py
+++ Rotation of an image.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 #reading an image in greyscale
-img = cv2.imread(r"C:\Users\GAMING-3\Documents\meet_my_electronic_pet_goofy.jpg", 0)
+img = cv2.imread(r"C:\Users\s44nidhya\Docs\meet_my_electronic_pet_goofy.jpg", 0)
 rows, cols = img.shape
 
 # Define rotation matrix to rotate the image by 90 degrees
