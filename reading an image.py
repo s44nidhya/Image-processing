36ed84f01a74691f74baf30f3fe9deec1365c53a
s44nidhya <140419811+s44nidhya@users.noreply.github.com>
2024-11-14 11:54:45 +0530

    Update reading an image.py

--- reading an image.py
+++ reading an image.py
@@ -1,6 +1,6 @@
 import cv2 as cv;
 #note that we are taking the global image path in the follwing code you can use local image path aswell
-img = cv.imread(r"C:\Users\GAMING-3\Documents\meet_my_electronic_pet_goofy.jpg")
+img = cv.imread(r"C:\Users\s44nidhya\Docs\meet_my_electronic_pet_goofy.jpg")
 
 cv.imshow('image', img)
 
