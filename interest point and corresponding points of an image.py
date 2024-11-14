2415cb54541f87b6ae32e5bb992a8a252f162119
s44nidhya <140419811+s44nidhya@users.noreply.github.com>
2024-11-14 11:54:14 +0530

    Update interest point and corresponding points of an image.py

--- interest point and corresponding points of an image.py
+++ interest point and corresponding points of an image.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 filename = 'meet_my_electronic_pet_goofy.jpg'
-img = cv2.imread(r"C:\Users\GAMING-3\Documents\meet_my_electronic_pet_goofy.jpg")
+img = cv2.imread(r"C:\Users\s44nidhya\Docs\meet_my_electronic_pet_goofy.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = np.float32(gray)
 dst = cv2.cornerHarris(gray, 2, 3, 0.04)
